dcf062ff87cb7bb00d3d76b14dd1f76a9fcf97b4
gitsnippet <pengtao@hncfs.edu.cn>
2018-11-24 18:04:57 +0800

    add files at  Sat Nov 24 18:04:57 HKT 2018

--- accessItem.php
+++ accessItem.php
@@ -1,17 +1,11 @@
 <?php
- $date = $_POST["date"];
- $content = $_POST["content"];
- $type = $_POST["type"];
- $meaning = $_POST["meaning"];
- $source = $_POST["source"];
-
+extract($_POST);
  $dsn = "mysql:host=localhost;dbname=english";
  $con = new PDO($dsn,"root","xyzzy");
- 
- global  $date,$content,$type,$meaning,$source,$dsn,$con;
 
 function checkData(){
-    global  $date,$content,$type,$meaning,$source,$dsn,$con;
+    global $_POST,$dsn,$con;
+    extract($_POST);
     $query_check = "select * from english where `content`='$content' and `type`='$type' ";
     $result = $con->query($query_check);
     $rows = $result->fetchAll();
@@ -19,7 +13,8 @@ function checkData(){
 }
 
 function writeData(){
-    global  $date,$content,$type,$meaning,$source,$dsn,$con;
+    global $_POST,$dsn,$con;
+    extract($_POST);
     $query_insert = "insert into english (`date`,`content`,`type`,`meaning`,`source`) 
                      values('$date','$content','$type','$meaning','$source')"; 
     if($con->query($query_insert)){
@@ -31,20 +26,21 @@ function writeData(){
 }
 
 function updateData(){
-    global  $date,$content,$type,$meaning,$source,$dsn,$con;
-    $query_update = "update english SET `date`='$date',`content`='$content',
-                                        `type`='$type',`meaning`='$meaning',`source`='$source' ";
+    global $_POST,$dsn,$con;
+    extract($_POST);
+    $query_update = "update english SET `date`='$date',`meaning`='$meaning',`source`='$source' where `content`='$content' and`type`='$type'";
     if($con->query($query_update)){
         setcookie("flag_update","",0);
     }
     else{
+        setcookie("flag_update","",0);
         echo "something wrong";
     }
 }
 
-if(isset($_COOKIE["flag_update"]) && $_COOKIE["flag_update"]  == 1  &&  isset($_POST)){
+if(isset($_COOKIE["flag_update"]) && $_COOKIE["flag_update"]  == 2  &&  isset($_POST)){
     updateData();
-    exit("update database");
+    exit();
 }
  
 $rows = checkData();
@@ -53,7 +49,6 @@ $rowCount = count($rows);
 if($rowCount >= 1){
     $result = $rows;
     setcookie("flag_update","1");
-//    echo "combine exist content and then update</br>";
     echo (json_encode($result));
 }
 elseif($rowCount == 0){
---
+++ ajax.js
@@ -0,0 +1,39 @@
+    function sendData()
+    {
+        var formData = new FormData( document.querySelector("form") );
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("post", "accessItem.php",true); 
+        xmlHttp.send(formData); 
+        xmlHttp.onreadystatechange = function(){        
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200){ 
+                var cookies = document.cookie;
+                var list = cookies.split("; ");         
+                for(var i = 0; i < list.length; i++) {
+                    arr = list[i].split("=");  
+                    if(arr[0] == "flag_update" &&  arr[1] == 1){
+                        data = xmlHttp.responseText;
+                        data = JSON.parse(data)[0];
+                        console.log(data.meaning);
+                        tmp_meaning = document.getElementById("meaning").value;
+                        console.log(tmp_meaning);
+                        data.meaning = data.meaning + "\n" + tmp_meaning;
+                        document.getElementById("meaning").value = data.meaning;
+                        tmp_source = document.getElementById("source").value;
+                        data.source = data.source + "\n" + tmp_source;
+                        document.getElementById("source").value = data.source;
+                        document.getElementById("prompt").innerHTML =  "已经合并已有的内容，请确认无误后，再次点击submit"       
+                        document.cookie="flag_update="+2;   
+                        return 0;
+                     }
+                }
+                document.getElementById("submit").value = "changed";
+                document.getElementById("submit").id = "changed";
+                document.getElementById("prompt").innerHTML = xmlHttp.responseText;
+                ob.removeEventListener("click",sendData);
+            }
+        }
+    }
+
+    ob = document.getElementById("submit"); 
+    ob.addEventListener("click",sendData)
+    
